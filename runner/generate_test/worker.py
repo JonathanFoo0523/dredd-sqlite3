e2987cc9417128f4989c002d06a587b4bdebd7b9
Jonathan Foo <jonathan.foo20@imperial.ac.uk>
2024-04-29 18:57:39 +0000

    Feat: TestGenerationWorker.differential_oracle

--- runner/generate_test/worker.py
+++ runner/generate_test/worker.py
@@ -6,8 +6,11 @@ import subprocess
 import tempfile
 import time
 
+SQLANCER_JAR_PATH = '/home/ubuntu/sqlancer/target/sqlancer-2.0.0.jar'
+
 class TestGenerationWorker:
-    def __init__(self, tracking_binary: str, mutation_binary: str):
+    def __init__(self, source_name: str, tracking_binary: str, mutation_binary: str):
+        self.source_name = source_name
         self.tracking_binary = tracking_binary
         self.mutation_binary = mutation_binary
 
@@ -30,10 +33,28 @@ class TestGenerationWorker:
         
         return set(covered_mutants), proc_result
 
-    def generate_random_testcases(self):
-        pass
 
-    # return True is results is probably deterministics and mutated sqlite give different result compared to unmutated
+    # Note that this generate 100 database at a time, with seeds [random_seed, random_seed+100)
+    def generate_random_testcases(self, random_seed: int, temp_dir: str, num_queries: int = 1000, oracle: str= 'FUZZER') -> None:
+        try:
+            proc = subprocess.run([
+                        'java',
+                        '-jar',
+                        SQLANCER_JAR_PATH,
+                        '--random-seed',
+                        str(random_seed),
+                        '--num-queries',
+                        str(num_queries),
+                        '--max-generated-databases',
+                        '1',
+                        'sqlite3',
+                        '--oracle',
+                        oracle
+                    ], cwd=temp_dir, stdout=subprocess.DEVNULL)
+        except Exception as err:
+            print(err)
+
+    # return True if results is probably deterministics and mutated sqlite give different result compared to unmutated
     # Consider timeout on mutated version  as different
     def differential_oracle(self, mutation_id: int, statements_path: str, expected_result=None) -> bool:
         with open(statements_path, 'rb') as f:
@@ -57,19 +78,25 @@ class TestGenerationWorker:
         return proc_ref.returncode != proc_mut.returncode or proc_ref.stdout != proc_mut.stdout or proc_ref.stderr != proc_mut.stderr
 
     
+    def slice_runner(self):
+        pass
 
     
 
+# with tempfile.NamedTemporaryFile(prefix='sqlite-test-temp') as logfile:
+#     logfile.write('CREATE TABLE a(b);\n'.encode())
+#     logfile.write('ALTER TABLE a RENAME TO c;\n'.encode())
+#     logfile.write("UPDATE c SET(d, d) = (7, 'c8320d45');\n".encode())
 
-with tempfile.NamedTemporaryFile(prefix='sqlite-test-temp') as logfile:
-    logfile.write('CREATE TABLE a(b);\n'.encode())
-    logfile.write('ALTER TABLE a RENAME TO c;\n'.encode())
-    logfile.write("UPDATE c SET(d, d) = (7, 'c8320d45');\n".encode())
+#     logfile.seek(0)
 
-    logfile.seek(0)
+#     res = TestGenerationWorker('/home/ubuntu/dredd-sqlite3/sample_binary/sqlite3_alter_tracking', '/home/ubuntu/dredd-sqlite3/sample_binary/sqlite3_alter_mutations').differential_oracle(69, logfile.name)
+#     print(res)
 
-    res = TestGenerationWorker('/home/ubuntu/dredd-sqlite3/sample_binary/sqlite3_alter_tracking', '/home/ubuntu/dredd-sqlite3/sample_binary/sqlite3_alter_mutations').differential_oracle(69, logfile.name)
-    print(res)
+with tempfile.TemporaryDirectory() as temp_dir:
+    worker = TestGenerationWorker('alter', '/home/ubuntu/dredd-sqlite3/sample_binary/sqlite3_alter_tracking', '/home/ubuntu/dredd-sqlite3/sample_binary/sqlite3_alter_mutations')
+    worker.generate_random_testcases(1, temp_dir)
+    print(sorted(os.listdir(f'{temp_dir}/logs/sqlite3')))
 
 
 # killed = set([from previous])
