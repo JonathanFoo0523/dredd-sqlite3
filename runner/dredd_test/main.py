f01240e0bdc4b069af358c49bf3cec80d542fb86
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2024-05-07 19:34:14 +0100

    Feat: remove hardcoded path

--- runner/dredd_test/main.py
+++ runner/dredd_test/main.py
@@ -4,26 +4,12 @@ from runner.dredd_source.main import sqlite_c_src_c_files
 from runner.dredd_test.worker import MutationTestingWorker
 
 from multiprocessing import Pool
-from tqdm.auto import tqdm
 import asyncio
 import os
 import pickle
 
-def worker(file: str, output_dir: str):
-    file = file.split('.')[0]
 
-    with open('/home/ubuntu/dredd-sqlite3/sample_binary/ss_tests.txt') as test_files:
-        tests = ['/home/ubuntu/sqlite-src/' + line.rstrip('\n') for line in test_files]
-        # tests = ['/home/ubuntu/sqlite-src/test/tkt3630.test']
-
-        coverage_bin = f'/home/ubuntu/dredd-sqlite3/sample_binary2/testfixture_{file}_coverage'
-        mutation_bin = f'/home/ubuntu/dredd-sqlite3/sample_binary2/testfixture_{file}_mutation'
-        mutation_info = f'/home/ubuntu/dredd-sqlite3/sample_binary2/{file}_mutation_info.json'
-
-        asyncio.run(MutationTestingWorker(file, coverage_bin, mutation_bin, mutation_info, output_dir).async_slice_runner(tests))
-
-
-if __name__ == '__main__':
+def main(mutation_binary_dir: str, output_dir: str, dredd_mutant_info_script_path: str):
     # print(f"Starting multiproceses with {cpu_count()} worker")
     # pool = Pool(processes=4)
     # with Pool(8) as pool:
@@ -45,6 +31,21 @@ if __name__ == '__main__':
     sqlite_c_src_c_files = sorted(list(set(sqlite_c_src_c_files) - set(source_file_covered)))
 
     for file in sqlite_c_src_c_files:
-        worker(file, output_dir)
+        file = file.split('.')[0]
+
+        with open('/home/ubuntu/dredd-sqlite3/sample_binary/ss_tests.txt') as test_files:
+            tests = ['/home/ubuntu/sqlite-src/' + line.rstrip('\n') for line in test_files]
+            # tests = ['/home/ubuntu/sqlite-src/test/tkt3630.test']
+
+            coverage_bin = os.path.join(mutation_binary_dir, f'testfixture_{file}_coverage')
+            mutation_bin = os.path.join(mutation_binary_dir, f'testfixture_{file}_mutation')
+            mutation_info = os.path.join(mutation_binary_dir, f'{file}_mutation_info.json')
+
+            asyncio.run(MutationTestingWorker(file, coverage_bin, mutation_bin, mutation_info, output_dir).async_slice_runner(tests))
     # for i in tqdm(pool.imap(worker, sqlite_c_src_c_files), total=len(sqlite_c_src_c_files), position=0, leave=False):
     #     pass
+
+
+
+if __name__ == '__main__':
+    main()
