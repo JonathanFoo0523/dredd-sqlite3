039bcc6152d0e7f72680aa9c54982b15bf704a36
Ubuntu <ubuntu@ip-172-31-46-117.eu-west-2.compute.internal>
2024-05-19 10:46:18 +0000

    Feat: asyncio reduce-test

--- runner/reduce_test/worker.py
+++ runner/reduce_test/worker.py
@@ -7,11 +7,13 @@ import asyncio
 import jinja2
 import tempfile
 import stat
+import shutil
+import subprocess
 
 class TestReductionWorker:
-    def __init__(self, mutation_binary, generation_directory, output_dir, max_parallel_tasks=4):
+    def __init__(self, mutation_binary, generation_directory, output_dir, max_parallel_tasks=1):
         self.mutation_binary = os.path.abspath(mutation_binary)
-        self.generation_directory = generation_directory
+        self.generation_directory = os.path.abspath(generation_directory)
         self.output_dir = output_dir
         self.max_parallel_tasks = max_parallel_tasks
 
@@ -20,28 +22,36 @@ class TestReductionWorker:
         while True:
             source, mutant, testcase = await queue.get()
 
+            if not os.path.isdir(os.path.join(self.output_dir, source)):
+                os.mkdir(os.path.join(self.output_dir, source))
+
             interestingness_test_template = jinja2.Environment(
             loader=jinja2.FileSystemLoader(
                 searchpath=os.path.dirname(os.path.realpath(__file__)))).get_template("interesting.py.jinja")
 
-            # with tempfile.TemporaryDirectory() as tempdir:
-            tempdir = self.output_dir
-            open(os.path.join(tempdir, 'interesting.py'), 'w').write(interestingness_test_template.render(
-                sqlite3_mutation_binary=os.path.join(self.mutation_binary, f'sqlite3_{source}_mutation'),
-                mutation_id = mutant,
-                testcase_to_check = f'database_{testcase}.log',
-                timeout_multiplier = TIMEOUT_MULTIPLIER_FOR_DIFFERENTIAL_TEST,
-            ))
+            with tempfile.TemporaryDirectory() as tempdir:
+                open(os.path.join(tempdir, 'interesting.py'), 'w').write(interestingness_test_template.render(
+                    sqlite3_mutation_binary=os.path.join(self.mutation_binary, f'sqlite3_{source}_mutation'),
+                    mutation_id = mutant,
+                    testcase_to_check = f'database_{testcase}.log',
+                    timeout_multiplier = TIMEOUT_MULTIPLIER_FOR_DIFFERENTIAL_TEST,
+                ))
 
-            # Make the interestingness test executable
-            st = os.stat(os.path.join(tempdir, 'interesting.py'))
-            os.chmod(os.path.join(tempdir, 'interesting.py'), st.st_mode | stat.S_IEXEC)
+                # shutil.copy(os.path.join(tempdir, 'interesting.py'), os.path.join(self.output_dir, source, f'interesting_{mutant}.py'))
 
-            # Execute 
-            statements_path = os.path.join(self.generation_directory, 'interesting_test_dir', f'database_{testcase}.log')
-            proc = await subprocess_run(['creduce', 'interesting.py', statements_path], cwd=tempdir)
-            print(proc)
-            exit(0)
+                # Make the interestingness test executable
+                st = os.stat(os.path.join(tempdir, 'interesting.py'))
+                os.chmod(os.path.join(tempdir, 'interesting.py'), st.st_mode | stat.S_IEXEC)
+
+                statements_path = os.path.join(self.generation_directory, 'interesting_test_dir', f'database_{testcase}.log')
+                shutil.copy(statements_path, os.path.join(tempdir, f'database_{testcase}.log'))
+
+                # Execute 
+                proc = await subprocess_run(['creduce', 'interesting.py', f'database_{testcase}.log'], cwd=tempdir)
+                if proc[2] != 0:
+                    raise Exception(f'creduce failed, Mutant {mutant}, Testcase {testcase}')
+
+                shutil.copy(os.path.join(tempdir, f'database_{testcase}.log'), os.path.join(self.output_dir, source, f'testcase_{mutant}.log'))
 
 
     async def runner(self):
@@ -70,7 +80,5 @@ class TestReductionWorker:
 
         for task in consumers:
             task.cancel()
-
-
 
         # print(generation_result)
