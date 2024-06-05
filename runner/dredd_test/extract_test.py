cae661ee15b25e8c6127ab7132f73fe4a1fec0fe
Jonathan Foo <jonathanfoo0523@gmail.com>
2024-06-05 15:10:29 +0000

    Feat: add timeout for extract test

--- runner/dredd_test/extract_test.py
+++ runner/dredd_test/extract_test.py
@@ -3,6 +3,7 @@ import subprocess
 import os
 import re
 from queue import PriorityQueue
+from runner.common.constants import TIMEOUT_FOR_TEST_EXTRACTION_SECONDS
 
 class TestExtractor:
     def __init__(self, sqlite_src, testfixture_path, subset, max_parallel_tasks=4):
@@ -15,23 +16,32 @@ class TestExtractor:
         result = []
         with tempfile.TemporaryDirectory(prefix='dredd-sqlite3-dredd-test-extractor') as tempdir:
             testrunner_path = os.path.join(self.sqlite_src, 'test', 'testrunner.tcl')
-            proc = subprocess.run([self.testfixture_path, testrunner_path, self.subset, '--jobs', str(self.max_parallel_tasks)], stdout=subprocess.DEVNULL, cwd=tempdir)
-
-            if proc.returncode != 0:
-                raise Exception(f"TestExtractor: testrunner failed with excitcode: {proc.returncode}")
+            try:
+                proc = subprocess.run([self.testfixture_path, testrunner_path, self.subset, '--jobs', str(self.max_parallel_tasks)], stdout=subprocess.DEVNULL, cwd=tempdir, timeout=TIMEOUT_FOR_TEST_EXTRACTION_SECONDS)
+            except subprocess.TimeoutExpired:
+                if proc:
+                    proc.terminate()
+                # pass
+            else:
+                if proc.returncode != 0:
+                    raise Exception(f"TestExtractor: testrunner failed with excitcode: {proc.returncode}")
 
             pq = PriorityQueue()
     
             with open(os.path.join(tempdir, 'testrunner.log'), 'r') as f:
                 for line in f.readlines():
                     if '###' not in line:
-                            continue
-                    match = re.search(r'###( config=.*)? (.*) (\d+)ms \(done\)', line.rstrip('\n'))
+                        continue
+                    match = re.search(r'###( config=.*)? (.*) (\d+)ms \((.*)\)', line.rstrip('\n'))
                     try:
                         filepath, time = (match.group(2), match.group(3))
+                        status = match.group(4)
                     except Exception as err:
-                        print(line, match.group(0))
+                        print("Cant match:", line)
                         raise err
+
+                    if status != 'done':
+                        raise Exception(f"TestExtractor: {filepath} failed without mutation")
 
                     pq.put((int(time), filepath))
 
